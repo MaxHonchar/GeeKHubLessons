5119879fc50e0638790035b1e62a0998028a195a
MaxHonchar <max.honchar@gmail.com>
2013-10-16 22:40:01 +0300

    refactoring and add 2 test
    1 test is for equals two users
    2 test is for equals contains data in map

--- lessons/src/test/MainTests.groovy
+++ lessons/src/test/MainTests.groovy
@@ -10,18 +10,36 @@ import ua.max.test.modal.User
  * To change this template use File | Settings | File Templates.
  */
 class MainTests extends GroovyTestCase {
-    void testUserListSize() {
-        def userList = [];
-        assert userList.size() == 0 : "list is not empty";
-        def range = 0..5;
-        range.each { it ->
-            userList.add(new User(name: "Max$it", surname: "Honchar"));
-        }
 
-        assert userList.get(3).name == "Max3":"this element doesn't exist in list";
-
-
+    void testIsUser() {
+        String name = "Max";
+        String surname = "Honchar";
+        def user = new User(name, surname);
+        def userList = [name: name, surname: surname, user: user];
+        assert userList instanceof java.util.Map: "this value isn't Map";
+        assert userList.containsValue(user);
     }
+
+    void testUserListSizeAndContainsElement() {
+        def userMap = [];
+        assert userMap.size() == 0: "list is not empty";
+        def range = 0..5;
+        range.each {
+            userMap.add(new User(name: "Max$it", surname: "Honchar"));
+        }
+        assert userMap.get(3).name == "Max3": "this element doesn't exist in list";
+    }
+
+    void testEqualsUser() {
+        String name = "Bruce";
+        String surname = "Wayne";
+        User userFirst = new User(name, surname);
+        name = "Batman"
+        User userSecond = new User(name, "");
+        assertFalse(userFirst.equals(userSecond));
+    }
+
+
 }
 
 
