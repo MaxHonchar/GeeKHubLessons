546567783f258ba29c91a89aebcf9fff34104712
MaxHonchar <max.honchar@gmail.com>
2013-10-24 17:36:01 +0300

    home task 2 phase 2
    refactoring, write in file

--- lessons/src/test/homeTask2/HomeTask2.groovy
+++ lessons/src/test/homeTask2/HomeTask2.groovy
@@ -2,6 +2,73 @@ package test.homeTask2
 
 class HomeTask2 {
 
+    def inputMass(def mass, def random) {
+        (0..<mass.size()).each { i ->
+            (0..<mass[i].size()).each { j ->
+                mass[i][j] = 1 + random.nextInt(5);
+            }
+        }
 
+        return mass;
+    }
+
+    def sumDiagonalsOfMatrix(def mass) {
+        def sum = 0;
+        (0..<mass.size()).each { i ->
+            sum += mass[i][i]
+        }
+
+        (mass.size() - 1..<0).each { i ->
+            sum += mass[i][i]
+        }
+
+        return sum;
+    }
+
+    def getCountByLetter(def str, def letter) {
+        def count = 0;
+        str.each {
+            if (it == letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    def outputMass(def mass) {
+        (0..<mass.size()).each { i ->
+            println()
+            (0..<mass[i].size()).each { j ->
+                print(mass[i][j] + " ")
+            }
+        }
+    }
+
+    def parseDate(Date date, def format) {
+        def formattedDate;
+        formattedDate = date.format(format)
+        println(formattedDate)
+        return formattedDate
+    }
+
+    def writeIntoFile(def title, def str, def basePatch) {
+
+        File file = new File(basePatch + "\\" + title)
+        if (file.exists()) {
+            assert file.delete()
+            assert file.createNewFile()
+        }
+
+        boolean append = true
+        FileWriter fileWriter = new FileWriter(file, append)
+        BufferedWriter buffWriter = new BufferedWriter(fileWriter)
+
+        buffWriter.write str.toString()
+
+        buffWriter.flush()
+        buffWriter.close()
+
+        file.renameTo( new File(basePatch + "\\" + "done_" +title) )
+    }
 
 }
--- lessons/src/ua/max/test/modal/Main.groovy
+++ lessons/src/ua/max/test/modal/Main.groovy
@@ -1,6 +1,7 @@
 package ua.max.test.modal
 
 import groovy.io.FileType
+import test.homeTask2.HomeTask2
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,23 +11,21 @@ import groovy.io.FileType
  * To change this template use File | Settings | File Templates.
  */
 
-/*
-def surname = "Honchar";
-String name = "Max";
-surname = "$surname";
-User user = new User(name: name, surname: surname);
-println user;*/
-
-
-
 def list = []
-
 def basePatch =  new File(".").getCanonicalPath()
 def resourcePath = "\\resources\\homeTask2"
 def regExpUrl = "\\ua\\max\\test\\modal"
 def firstCase = "a_"
 def secondCase = "1_"
 def thirdCase = "d_"
+def size = 4;
+def random = new Random();
+def formatDate = "YYYY-MM-DD HH-MM-SS";
+def date = new Date();
+def mass = new Integer[size][size];
+def sum = 0;
+
+HomeTask2 homeTask2 = new HomeTask2();
 basePatch = basePatch - regExpUrl + resourcePath
 
 def dir = new File(basePatch)
@@ -35,10 +34,19 @@ dir.eachFileRecurse (FileType.FILES) { file ->
 }
 
 list.each {
-    println(it)
     switch (it[0] + it[1]) {
-        case firstCase:println("1");break;
-        case secondCase:println("2");break;
-        case thirdCase:println("3");break;
+        case firstCase:
+            def count = homeTask2.getCountByLetter(it, "a");
+            homeTask2.writeIntoFile(it, count, basePatch);
+            break;
+        case secondCase:
+            mass = homeTask2.inputMass(mass, random);
+            sum = homeTask2.sumDiagonalsOfMatrix(mass);
+            homeTask2.writeIntoFile(it, sum, basePatch);
+            break;
+        case thirdCase:
+            def formatedDate = homeTask2.parseDate(date, formatDate);
+            homeTask2.writeIntoFile(it, formatedDate, basePatch);
+            break;
     }
 }
