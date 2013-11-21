2bb28aa9fb6b728f0ce8333e58f1708a3484606d
MaxHonchar <max.honchar@gmail.com>
2013-11-21 12:08:33 +0200

    AstTransformation, refactoring
    create ASTTransformation, refactoring, spock

---
+++ lessons/src/test/homeTask3/LocalASTTransformation.groovy
@@ -0,0 +1,45 @@
+package test.homeTask3
+
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.ast.expr.ArgumentListExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.stmt.ExpressionStatement
+import org.codehaus.groovy.control.CompilePhase
+import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.transform.ASTTransformation
+import org.codehaus.groovy.transform.GroovyASTTransformation
+import org.objectweb.asm.tree.MethodNode
+
+import java.beans.Statement
+
+@GroovyASTTransformation(phase= CompilePhase.CONVERSION)
+public class LocalASTTransformation implements ASTTransformation  {
+
+    static final def TARGET = WithLogging.getName()
+
+    public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
+        List methods = sourceUnit.getAST()?.getMethods()
+        methods?.each { MethodNode method ->
+            Statement startMessage = createPrintlnAst("Starting $method.name")
+            Statement endMessage = createPrintlnAst("Ending $method.name")
+
+            List existingStatements = method.getCode().getStatements()
+            existingStatements.add(0, startMessage)
+            existingStatements.add(endMessage)
+        }
+    }
+
+    private Statement createPrintlnAst(String message) {
+        return new ExpressionStatement(
+                new MethodCallExpression(
+                        new VariableExpression("this"),
+                        new ConstantExpression("println"),
+                        new ArgumentListExpression(
+                                new ConstantExpression(message)
+                        )
+                )
+        )
+    }
+}
